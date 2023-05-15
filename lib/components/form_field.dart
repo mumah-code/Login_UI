6ca66ece0d7e9dd75e0e0eae9d0de0b6347bdb38
Md._Mubin <mumah786@gmail.com>
2023-05-15 17:56:28 +0000

    textform

--- lib/components/form_field.dart
+++ lib/components/form_field.dart
@@ -13,7 +13,7 @@ class MyTextfield extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      padding: const EdgeInsets.symmetric(horizontal: 40.0),
       child: TextField(
         controller: controller,
         obscureText: true,
