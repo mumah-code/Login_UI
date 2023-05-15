d6d273db8a7d9f54bc8b9d7a482bfac80b76de57
Md._Mubin <mumah786@gmail.com>
2023-05-15 17:24:26 +0000

    adding icon, text, textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:login/components/form_field.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
   // This widget is the root of your application.
+  // text_editing controller
+  final usernameController = TextEditingController();
+  final passWord = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Flutter Login',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Login Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -32,10 +37,35 @@ class MyHomePage extends StatelessWidget {
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          children: [
+            // icon
+            const Icon(
+              Icons.login,
+              size: 100,
+            ),
+            // welcome_text
+            const Text(
+              'Welcome to Our Page',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 30,
+              ),
+            ),
+            MyTextfield(
+              controller: usernameController,
+              hintText: 'UserName',
+              obscureText: false,
+            ),
+          ],
+        )
+
+            // text_form_field
+            //forgot passwprd
+            //
+            ),
       ),
     );
   }
